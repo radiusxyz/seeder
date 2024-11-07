116e6484dc6e2a6c17db746e3c8ac47a9946380f
jaemin <datactress@gmail.com>
2024-11-07 19:02:54 +0900

    feat: change GetSequencerRpcUrlList(add wrapper)

--- src/rpc/external/get_sequencer_rpc_url_list.rs
+++ src/rpc/external/get_sequencer_rpc_url_list.rs
@@ -6,8 +6,15 @@ pub struct GetSequencerRpcUrlList {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct SequencerRpcInfo {
+    pub address: String,
+    pub external_rpc_url: String,
+    pub cluster_rpc_url: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct GetSequencerRpcUrlListResponse {
-    pub sequencer_rpc_url_list: Vec<(String, Option<(String, String)>)>,
+    pub sequencer_rpc_url_list: Vec<SequencerRpcInfo>,
 }
 
 impl GetSequencerRpcUrlList {
@@ -19,20 +26,17 @@ impl GetSequencerRpcUrlList {
     ) -> Result<GetSequencerRpcUrlListResponse, RpcError> {
         let parameter = parameter.parse::<Self>()?;
 
-        let sequencer_rpc_url_list: Vec<(String, Option<(String, String)>)> = parameter
+        let sequencer_rpc_url_list: Vec<SequencerRpcInfo> = parameter
             .sequencer_address_list
             .into_iter()
-            .map(|address| {
-                let sequencer_rpc_url = SequencerNodeInfo::get(&address)
-                    .map(|node_info| {
-                        (
-                            node_info.external_rpc_url().to_owned(),
-                            node_info.cluster_rpc_url().to_owned(),
-                        )
+            .filter_map(|address| {
+                SequencerNodeInfo::get(&address)
+                    .ok()
+                    .map(|node_info| SequencerRpcInfo {
+                        address: address.as_hex_string(),
+                        external_rpc_url: node_info.external_rpc_url().to_owned(),
+                        cluster_rpc_url: node_info.cluster_rpc_url().to_owned(),
                     })
-                    .ok();
-
-                (address.as_hex_string(), sequencer_rpc_url)
             })
             .collect();
 
